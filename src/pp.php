cd22f550bf1b7474efec008f14d589f5afbb5702
edmiguens <edmiguens@gmail.com>
2025-07-17 16:17:23 -0400

    Delete src directory

--- src/pp.php
+++
@@ -1,4 +0,0 @@
-echo "<pre>";
-echo "URL generada:\n" . $OAuth2LoginHelper->getAuthorizationCodeURL();
-echo "</pre>";
-exit;
--- src/test.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo file_exists("clientes.php") ? "✅ clientes.php OK<br>" : "❌ Falta<br>";
-echo file_exists("token.json") ? "✅ token.json OK<br>" : "❌ Falta<br>";
-echo file_exists("Conexion2.php") ? "✅ Conexion2.php OK<br>" : "❌ Falta<br>";
-?>
